f3c362736f61f0e1e00238a57800542e7a63cb87
André Mossige <mossige.a@gmail.com>
2020-03-17 13:34:55 +0100

    Init prosjekt

---
+++ src/main/java/no/nav/rekrutteringsbistand/sms/rekrutteringsbistandsms/RekrutteringsbistandSmsApplication.java
@@ -0,0 +1,13 @@
+package no.nav.rekrutteringsbistand.sms.rekrutteringsbistandsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RekrutteringsbistandSmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RekrutteringsbistandSmsApplication.class, args);
+	}
+
+}
