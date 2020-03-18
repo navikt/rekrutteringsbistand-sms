ab4f7738fa6fc693e94029cbe0e0c82f8c97b9d8
André Mossige <mossige.a@gmail.com>
2020-03-18 09:30:06 +0100

    Legg til lokal login

---
+++ src/main/java/no/nav/rekrutteringsbistand/sms/rekrutteringsbistandsms/mock/LokalLoginConfig.java
@@ -0,0 +1,12 @@
+package no.nav.rekrutteringsbistand.sms.rekrutteringsbistandsms.mock;
+
+import no.nav.security.token.support.test.spring.TokenGeneratorConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Import(TokenGeneratorConfiguration.class)
+@Profile("dev")
+public class LokalLoginConfig {
+}
