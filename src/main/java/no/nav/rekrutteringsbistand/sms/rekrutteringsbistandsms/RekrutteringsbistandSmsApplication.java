2a0359a74175811b44379bca73fe86cecd37c3da
André Mossige <mossige.a@gmail.com>
2020-03-18 09:20:22 +0100

    Legg til autentisering

--- src/main/java/no/nav/rekrutteringsbistand/sms/rekrutteringsbistandsms/RekrutteringsbistandSmsApplication.java
+++ src/main/java/no/nav/rekrutteringsbistand/sms/rekrutteringsbistandsms/RekrutteringsbistandSmsApplication.java
@@ -1,11 +1,13 @@
 package no.nav.rekrutteringsbistand.sms.rekrutteringsbistandsms;
 
+import no.nav.security.token.support.spring.api.EnableJwtTokenValidation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties
+@EnableJwtTokenValidation(ignore = "org.springframework")
 public class RekrutteringsbistandSmsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/no/nav/rekrutteringsbistand/sms/rekrutteringsbistandsms/SmsController.java
+++ src/main/java/no/nav/rekrutteringsbistand/sms/rekrutteringsbistandsms/SmsController.java
@@ -1,10 +1,12 @@
 package no.nav.rekrutteringsbistand.sms.rekrutteringsbistandsms;
 
+import no.nav.security.token.support.core.api.Protected;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Protected
 @RestController
 public class SmsController {
 
