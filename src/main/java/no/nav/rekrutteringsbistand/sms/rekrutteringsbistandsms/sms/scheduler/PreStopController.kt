298dbe52355e149240c8573c5096db7ab71960b9
André Mossige <mossige.a@gmail.com>
2020-03-24 13:28:47 +0100

    Prøv å delay i 30 sekunder i prestophook

--- src/main/java/no/nav/rekrutteringsbistand/sms/rekrutteringsbistandsms/sms/scheduler/PreStopController.kt
+++ src/main/java/no/nav/rekrutteringsbistand/sms/rekrutteringsbistandsms/sms/scheduler/PreStopController.kt
@@ -1,19 +1,25 @@
 package no.nav.rekrutteringsbistand.sms.rekrutteringsbistandsms.sms.scheduler
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import no.nav.rekrutteringsbistand.sms.rekrutteringsbistandsms.utils.log
 import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import kotlin.time.ExperimentalTime
+import kotlin.time.seconds
 
 @RestController
 class PreStopController(private val sendSmsScheduler: SendSmsScheduler) {
 
+    @ExperimentalTime
     @Unprotected
     @GetMapping("/internal/preStopHook")
     fun preStophook(): ResponseEntity<Unit> {
         log.info("Applikasjonen har fått beskjed om å stoppe, stopper skedulert SMS-utsending")
         sendSmsScheduler.stopSkedulertSmsutsending()
+        runBlocking { delay(30.seconds) }
         return ResponseEntity.ok().build()
     }
 }
