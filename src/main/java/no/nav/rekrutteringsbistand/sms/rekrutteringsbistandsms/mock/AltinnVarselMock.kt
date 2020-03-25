533d9826d7aad5d26e0d17aed6cb12f4895bc72c
André Mossige <mossige.a@gmail.com>
2020-03-25 13:36:26 +0100

    Legg til varsel om at utsending er slått av i dev

--- src/main/java/no/nav/rekrutteringsbistand/sms/rekrutteringsbistandsms/mock/AltinnVarselMock.kt
+++ src/main/java/no/nav/rekrutteringsbistand/sms/rekrutteringsbistandsms/mock/AltinnVarselMock.kt
@@ -5,10 +5,12 @@ import kotlinx.coroutines.runBlocking
 import no.altinn.schemas.services.serviceengine.notification._2015._06.SendNotificationResultList
 import no.altinn.schemas.services.serviceengine.standalonenotificationbe._2009._10.StandaloneNotificationBEList
 import no.altinn.services.serviceengine.notification._2010._10.INotificationAgencyExternalBasic
+import no.nav.rekrutteringsbistand.sms.rekrutteringsbistandsms.utils.log
 
 class AltinnVarselMock : INotificationAgencyExternalBasic {
     override fun sendStandaloneNotificationBasicV3(p0: String?, p1: String?, p2: StandaloneNotificationBEList?): SendNotificationResultList {
         runBlocking { delay(500) }
+        log.info("Sending av SMS er slått av i dev. Faker utsending...")
         return SendNotificationResultList()
     }
 
