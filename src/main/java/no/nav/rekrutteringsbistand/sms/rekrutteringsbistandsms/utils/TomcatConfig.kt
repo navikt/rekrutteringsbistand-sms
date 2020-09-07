a5d7de41eae0f8f05da66e7698a754756f5dd64e
André Mossige <mossige.a@gmail.com>
2020-09-07 17:16:49 +0200

    Ikke logg cookies som ikke følger cookie spec-en

---
+++ src/main/java/no/nav/rekrutteringsbistand/sms/rekrutteringsbistandsms/utils/TomcatConfig.kt
@@ -0,0 +1,23 @@
+package no.nav.rekrutteringsbistand.sms.rekrutteringsbistandsms.utils
+
+import org.apache.catalina.Context
+import org.apache.tomcat.util.http.LegacyCookieProcessor
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
+import org.springframework.boot.web.server.WebServerFactoryCustomizer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+// Tomcat logger feilformaterte cookies by default. Dette er et sikkerhetshull.
+// Her fikser vi dette ved å bruke LegacyCookieProcessor, som ikke logger slike.
+// Se https://www.jvt.me/posts/2020/04/07/tomcat-cookie-disclosure/ for mer informasjon.
+@Configuration
+class TomcatConfig {
+    @Bean
+    fun cookieProcessorCustomizer(): WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+        return WebServerFactoryCustomizer { tomcatServletWebServerFactory ->
+            tomcatServletWebServerFactory
+                    .addContextCustomizers(TomcatContextCustomizer { context: Context -> context.cookieProcessor = LegacyCookieProcessor() })
+        }
+    }
+}
