27fc678148420fdf003eb98251bc679da88d6c23
Are Husby <are.husby@nav.no>
2021-01-11 10:58:19 +0100

    Reduser antall warnings i apploggen som vi rutinemessig ignorerer (#21)
    * Oppgrader avhengigheter
    
    * Angi Java 14 også i Github workflow
    
    * Sammenligne timestamps på en måte som funker både på Github, Windows lokalt og Mac lokalt.
    
    * Bruk Java 14 også ved deploy til dev

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM navikt/java:13
+FROM navikt/java:14
 COPY import-vault-token.sh /init-scripts
 COPY import-serviceuser.sh /init-scripts
 COPY /target/rekrutteringsbistand-sms-0.0.1-SNAPSHOT.jar app.jar
--- src/test/java/no/nav/rekrutteringsbistand/sms/rekrutteringsbistandsms/HentSmsStatuserTest.kt
+++ src/test/java/no/nav/rekrutteringsbistand/sms/rekrutteringsbistandsms/HentSmsStatuserTest.kt
@@ -4,6 +4,7 @@ import no.nav.rekrutteringsbistand.sms.rekrutteringsbistandsms.sms.SmsRepository
 import no.nav.rekrutteringsbistand.sms.rekrutteringsbistandsms.sms.SmsStatus
 import no.nav.rekrutteringsbistand.sms.rekrutteringsbistandsms.sms.Status
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.within
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -19,7 +20,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringExtension
-import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit.SECONDS
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -45,17 +46,17 @@ class HentSmsStatuserTest {
         Thread.sleep(1500)
 
         val respons: ResponseEntity<List<SmsStatus>> = restTemplate.exchange(
-                "$baseUrl/sms/${enSmsTilOppretting.kandidatlisteId}",
-                HttpMethod.GET,
-                HttpEntity(null, null),
-                object : ParameterizedTypeReference<List<SmsStatus>>() {}
+            "$baseUrl/sms/${enSmsTilOppretting.kandidatlisteId}",
+            HttpMethod.GET,
+            HttpEntity(null, null),
+            object : ParameterizedTypeReference<List<SmsStatus>>() {}
         )
         assertThat(respons.statusCode).isEqualTo(HttpStatus.OK)
         respons.body!!.forEachIndexed { index, smsStatus ->
             assertThat(smsStatus.id).isGreaterThan(0)
             assertThat(smsStatus.fnr).isEqualTo(enSmsTilOppretting.fnr[index])
-            assertThat(smsStatus.opprettet).isEqualToIgnoringSeconds(LocalDateTime.now())
-            assertThat(smsStatus.sendt).isEqualToIgnoringSeconds(LocalDateTime.now())
+            assertThat(smsStatus.opprettet).isCloseTo(now(), within(2, SECONDS))
+            assertThat(smsStatus.sendt).isCloseTo(now(), within(2, SECONDS))
             assertThat(smsStatus.status).isEqualTo(Status.SENDT)
         }
     }
@@ -63,10 +64,10 @@ class HentSmsStatuserTest {
     @Test
     fun `GET mot sms skal returnere 200 ok med tom liste hvis ingen SMSer finnes på kandidatlista`() {
         val respons: ResponseEntity<List<SmsStatus>> = restTemplate.exchange(
-                "$baseUrl/sms/${enSmsTilOppretting.kandidatlisteId}",
-                HttpMethod.GET,
-                HttpEntity(null, null),
-                object : ParameterizedTypeReference<List<SmsStatus>>() {}
+            "$baseUrl/sms/${enSmsTilOppretting.kandidatlisteId}",
+            HttpMethod.GET,
+            HttpEntity(null, null),
+            object : ParameterizedTypeReference<List<SmsStatus>>() {}
         )
         assertThat(respons.statusCode).isEqualTo(HttpStatus.OK)
         assertThat(respons.body!!).isEmpty()
