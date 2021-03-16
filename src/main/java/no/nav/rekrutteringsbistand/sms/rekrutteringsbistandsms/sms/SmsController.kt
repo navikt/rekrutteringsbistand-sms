58af1285cc6d4fd3029feff52644300725908cf9
Mads Lee Giil <mads.lee.giil@nav.no>
2021-03-16 10:37:30 +0100

    Logg når validering av sms til utsendelse feiler

--- src/main/java/no/nav/rekrutteringsbistand/sms/rekrutteringsbistandsms/sms/SmsController.kt
+++ src/main/java/no/nav/rekrutteringsbistand/sms/rekrutteringsbistandsms/sms/SmsController.kt
@@ -1,6 +1,7 @@
 package no.nav.rekrutteringsbistand.sms.rekrutteringsbistandsms.sms
 
 import no.nav.rekrutteringsbistand.sms.rekrutteringsbistandsms.utils.AuthUtils
+import no.nav.rekrutteringsbistand.sms.rekrutteringsbistandsms.utils.log
 import no.nav.security.token.support.core.api.Protected
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -8,10 +9,10 @@ import org.springframework.web.bind.annotation.*
 @Protected
 @RestController
 class SmsController(
-        private val smsRepository: SmsRepository,
-        private val sendSmsService: SendSmsService,
-        private val smsValidator: SmsValidator,
-        private val authUtils: AuthUtils
+    private val smsRepository: SmsRepository,
+    private val sendSmsService: SendSmsService,
+    private val smsValidator: SmsValidator,
+    private val authUtils: AuthUtils
 ) {
 
     @PostMapping("/sms")
@@ -19,15 +20,17 @@ class SmsController(
         val (ok, httpStatus, melding) = smsValidator.valider(sms)
         if (ok) {
             smsRepository.lagreSms(
-                    sms = sms,
-                    navident = authUtils.hentNavident()
+                sms = sms,
+                navident = authUtils.hentNavident()
             )
             sendSmsService.sendSmserAsync()
+        } else {
+            log.warn("Kunne ikke sende SMS tıl kandidater på kandidatliste med id ${sms.kandidatlisteId}, årsak: $melding")
         }
 
         return ResponseEntity
-                .status(httpStatus)
-                .body(melding)
+            .status(httpStatus)
+            .body(melding)
     }
 
     @GetMapping("/sms/{kandidatlisteId}")
