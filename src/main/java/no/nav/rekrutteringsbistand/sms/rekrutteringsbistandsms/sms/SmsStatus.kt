860d14ab234ab8efbca8f978ab3d2ab1e2be0bce
Joar Aurdal <joar.aurdal@nav.no>
2021-12-07 10:06:07 +0100

    Legg til navIdent i response og oppgrader til java 17

--- src/main/java/no/nav/rekrutteringsbistand/sms/rekrutteringsbistandsms/sms/SmsStatus.kt
+++ src/main/java/no/nav/rekrutteringsbistand/sms/rekrutteringsbistandsms/sms/SmsStatus.kt
@@ -2,10 +2,11 @@ package no.nav.rekrutteringsbistand.sms.rekrutteringsbistandsms.sms
 
 import java.time.LocalDateTime
 
-data class SmsStatus (
-        val id: Int,
-        val fnr: String,
-        val opprettet: LocalDateTime,
-        val sendt: LocalDateTime?,
-        val status: Status
+data class SmsStatus(
+    val id: Int,
+    val fnr: String,
+    val opprettet: LocalDateTime,
+    val sendt: LocalDateTime?,
+    val status: Status,
+    val navIdent: String
 )
